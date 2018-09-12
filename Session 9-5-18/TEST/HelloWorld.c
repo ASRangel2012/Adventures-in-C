e5c701f86c51df08d207618bf1c1ae1c382689e9
ASRangel2012 <ASRAngel2012@outlook.com>
2018-09-12 17:09:32 -0400

    C
    1st step

---
+++ Session 9-5-18/TEST/HelloWorld.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+Author: COP2220
+DATE:9/5/18
+Description: This program prints "Hello World" to the console
+*/
+
+
+int main()
+{
+    printf("Hello world!\n");  //print f print to the console
+    return 0; //this signals to the OS the smooth ending of this program.
+} //end of main
+
+
